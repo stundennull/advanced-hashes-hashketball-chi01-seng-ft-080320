4075cdbf4dc8544b94e596e3ab955468f2dcf0ea
Kyle Walters <kyle.p.walters@gmail.com>
2020-08-02 00:47:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,4 +190,16 @@ def player_stats(input)
   end
 end
 
-
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
