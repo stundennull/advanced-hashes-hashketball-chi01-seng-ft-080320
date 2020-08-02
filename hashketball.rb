f9a4c774a26901d68b10c2fd5b7e4c06424f663b
Kyle Walters <kyle.p.walters@gmail.com>
2020-08-02 00:47:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -175,4 +175,19 @@ def player_numbers(input)
   return output
 end
 
+def player_stats(input)
+  game_hash.each do |team, team_info|
+    team_info.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if input == player[:player_name]
+            player.delete(:player_name) # having player name inside the hash was a bad idea!
+            return player
+          end
+        end
+      end
+    end
+  end
+end
+
 
